50772561953657150739c46f50f1f1676dba8474
Kraegon <Julio34@gmail.com>
2014-01-14 15:30:12 +0100

    Highscore init

---
+++ IPR/Control/HighscoreReader.cs
@@ -0,0 +1,63 @@
+﻿using IPR.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Windows.Data.Json;
+using Windows.Storage;
+
+namespace IPR.Control
+{
+    /// <summary>
+    /// TODO: Add more queries.
+    /// </summary>
+    class HighscoreReader
+    {
+        private static JsonObject highscoreCompilation;
+        private static bool isInitialised = false;
+
+        public HighscoreReader()
+        { 
+            //Can't init asynchronously, init is checked in get method.
+        }
+
+        private async Task initAsync()
+        {
+            string rawText = String.Empty;
+            //Three steps neccesary to subvert access shenanigans
+            var installFolder = Windows.ApplicationModel.Package.Current.InstalledLocation;
+            var subfolder = await installFolder.GetFolderAsync("Assets\\");
+            var file = await subfolder.GetFileAsync("Highscores.json");
+
+            rawText = await FileIO.ReadTextAsync(file);                
+            highscoreCompilation = JsonObject.Parse(rawText)["stringRef"].GetObject();
+            isInitialised = true;
+        }
+        /// <summary>
+        /// TODO: Test it
+        /// </summary>
+        /// <param name="rank"></param>
+        /// <returns></returns>
+        public static HighscoreObj findByRank(int rank)
+        {
+            HighscoreObj retVal = null;
+            JsonObject highscore = highscoreCompilation[rank.ToString()].GetObject();
+            try
+            {
+                string name = highscore["Name"].GetString();
+                float distance = (float) highscore["Distance"].GetNumber();
+                string[] time = highscore["TimeTaken"].GetString().Split(':');
+                int hours = int.Parse(time[0]);
+                int minutes = int.Parse(time[1]);
+                TimeSpan timeTaken = new TimeSpan(hours, minutes, 0);
+            }
+            catch (KeyNotFoundException)
+            {
+                retVal = new HighscoreObj();
+            }
+            return retVal;
+        }
+    }
+}
---
+++ IPR/Model/HighscoreObj.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IPR.Model
+{
+    class HighscoreObj
+    {
+        public string Name { get; set; }
+        public float Distance { get; set; }
+        public TimeSpan TimeTaken { get; set; }
+
+        public HighscoreObj()
+        {
+            Name = string.Empty;
+            Distance = 0;
+            TimeTaken = new TimeSpan();
+        }
+
+        public HighscoreObj(string name, float distance, TimeSpan timeTaken)
+        {
+            Name = name;
+            Distance = distance;
+            TimeTaken = timeTaken; 
+        }
+    }
+}
